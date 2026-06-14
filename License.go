e64ec10b0c1884997869d798c9ae6f59af5f6708
MrLenuxDev <mrlenux0420@gmail.com>
2026-06-14 18:46:10 -0400

    Update License.go

--- License.go
+++ License.go
@@ -1,11 +1,39 @@
-# PLAN ONE PLATINUM
-Price: $100
-Key: One Month License
+package main
 
-# PLAN TWO DIAMOND
-Price: $200
-Key: One year License
+import (
+    "fmt"
+)
 
-# PLAN THREE MASTER
-Price: $500
-Key: Life Time License
+func main() {
+    var word string
+    var num int
+    
+    fmt.Print("Enter license number: ")
+    fmt.Scan(&num)
+    
+    fmt.Print("Enter plan type (PLATINUM/DIAMOND/MASTER): ")
+    fmt.Scan(&word)
+    
+    switch word {
+    case "PLATINUM":
+        fmt.Println("\n# PLAN ONE PLATINUM")
+        fmt.Println("Price: $100")
+        fmt.Println("Key: One Month License")
+        fmt.Printf("The number is: %d\n", num)
+        
+    case "DIAMOND":
+        fmt.Println("\n# PLAN TWO DIAMOND")
+        fmt.Println("Price: $200")
+        fmt.Println("Key: One Year License")
+        fmt.Printf("The number is: %d\n", num)
+        
+    case "MASTER":
+        fmt.Println("\n# PLAN THREE MASTER")
+        fmt.Println("Price: $500")
+        fmt.Println("Key: Life Time License")
+        fmt.Printf("The number is: %d\n", num)
+        
+    default:
+        fmt.Println("Invalid plan type")
+    }
+}
